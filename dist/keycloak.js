ba69c98c56de94f8791377f5640149a95460c691
joohong89 <joohong89@gmail.com>
2021-10-12 20:55:45 +0800

    added capacitor dependency

--- dist/keycloak.js
+++ dist/keycloak.js
@@ -14,7 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { Plugins, AppState } from '@capacitor/core';
+import { Browser } from '@capacitor/browser';
 
+const { App } = Plugins;
 
 (function(root, factory) {
     if ( typeof exports === 'object' ) {

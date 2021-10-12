d50255751b5dae0c31b304bf0d0e98b7f943bed1
joohong89 <joohong89@gmail.com>
2021-10-12 21:07:14 +0800

    removed capacitor import

--- dist/keycloak.js
+++ dist/keycloak.js
@@ -14,10 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Plugins, AppState } from '@capacitor/core';
-import { Browser } from '@capacitor/browser';
-
-const { App } = Plugins;
 
 (function(root, factory) {
     if ( typeof exports === 'object' ) {
